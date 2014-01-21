5be3c0abb3ae001876e6fa582f9fe4f75d9e3782
chris <wfsr@foxmail.com>
2014-01-21 15:47:09 +0800

    修复 toQueryString 的赋值错误

--- src/ui/lib.js
+++ src/ui/lib.js
@@ -500,7 +500,7 @@ define(function () {
      *             
      * @return {string} 解析结果字符串，其中值将被URI编码
      */
-    lib.toQueryString = lib.object = function toQueryString(object, base){
+    var toQueryString = function (object, base){
         var queryString = [];
 
         forIn(object, function (value, key) {
@@ -531,6 +531,7 @@ define(function () {
 
         return queryString.join('&');
     };
+    lib.toQueryString = lib.object.toQueryString = toQueryString;
 
     /* ========================== lib.string ========================== */
    
