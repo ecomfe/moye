58d62568002eefb5a7b6ea0e3e0d8fa370be5f4d
chris <wfsr@foxmail.com>
2014-04-08 00:11:24 +0800

    对外隐藏私有方法，调整文档注释

--- src/ui/Tabs.js
+++ src/ui/Tabs.js
@@ -11,140 +11,14 @@ define(function (require) {
     var lib = require('./lib');
     var Control = require('./Control');
     
-
     /**
-     * 选项卡组件
+     * 私有函数或方法
      * 
-     * @constructor
-     * @extends module:Control
-     * @requires Control
-     * @exports Tabs
+     * @type {Object}
+     * @namespace
+     * @name module:Tabs~privates
      */
-    var Tabs = Control.extend(/** @lends module:Tabs.prototype */{
-
-        /**
-         * 控件类型标识
-         * 
-         * @type {string}
-         * @private
-         */
-        type: 'Tabs',
-
-        /**
-         * 控件配置项
-         * 
-         * @name module:Tabs#options
-         * @type {Object}
-         * @property {boolean} options.disabled 控件的不可用状态
-         * @property {(string | HTMLElement)} options.main 控件渲染容器
-         * @property {string} options.prefix 控件class前缀，同时将作为main的class之一
-         * @property {number} selectedIndex 默认选中项的索引值
-         * @private
-         */
-        options: {
-
-            // 提示框的不可用状态，默认为false。处于不可用状态的提示框不会出现。
-            disabled: false,
-
-            // 控件渲染主容器
-            main: '',
-
-            // 控件class前缀，同时将作为main的class之一
-            prefix: 'ecl-ui-tabs',
-
-            // 默认选中项
-            selectedIndex: 0
-        },
-
-        /**
-         * 需要绑定 this 的方法名，多个方法以半角逗号分开
-         * 
-         * @type {string}
-         * @private
-         */
-        binds: 'onClick',
-
-        /**
-         * 控件初始化
-         * 
-         * @param {Object} options 控件配置项
-         * @see module:Tabs#options
-         * @private
-         */
-        init: function (options) {
-            this.main = lib.g(options.main);
-            this.labels = lib.q(options.prefix + '-labels', this.main)[0];
-            this.selectedIndex = options.selectedIndex | 0;
-        },
-
-
-        /**
-         * 绘制控件
-         * 
-         * @public
-         */
-        render: function () {
-            var options = this.options;
-
-            if (!this.rendered) {
-                this.rendered = true;
-
-                // 先给选项卡父容器添加事件监听
-                lib.on(this.labels, 'click', this.onClick);
-
-                // 找出所有选项卡
-                var labels =
-                    lib.toArray(this.labels.getElementsByTagName('li'));
-
-                // 是否支持伪元素
-                var noPseudoElement = lib.browser.ie6;
-                lib.each(
-                    labels,
-                    function (label, i) {
-                        // 自动加上索引标识
-                        label.setAttribute('data-index', i);
-
-                        // 对于不支持伪元素的 ie6，自动插入一个 <i> 标签
-                        // TODO: innerHTML vs dom clone
-                        if (noPseudoElement) {
-                            label.innerHTML += '<i></i>';
-                        }
-                    }
-                );
-
-                var selectedClass = options.prefix + '-selected';
-                var selectedLabel = lib.q(selectedClass, this.labels)[0];
-
-                this.labels = labels;
-
-                if (selectedLabel) {
-                    this.selectedIndex = 
-                        selectedLabel.getAttribute('data-index') | 0;
-                }
-                else {
-                    selectedLabel = labels[this.selectedIndex];
-                    selectedLabel && lib.fire(selectedLabel, 'click');
-                }
-
-            }
-
-            return this;
-        },
-
-        /**
-         * 切换选项卡前判定
-         * 
-         * 主要用于业务逻辑判断是否允许切换选项卡，默认允许，不允许则需要明确返回 false
-         * 
-         * @param {number} oldIndex 原选中项索引值
-         * @param {number} newIndex 新选中项索引值
-         * @return {boolean} 是否允许切换
-         * @public
-         */
-        /* jshint unused:false */
-        onBeforeChange: function (oldIndex, newIndex) {
-            return true;
-        },
+    var privates = /** @lends module:Tabs~privates */ {
 
         /**
          * 点击事件处理
@@ -206,6 +80,135 @@ define(function (require) {
             );
             this.selectedIndex = index;
             
+        }
+    };
+
+    /**
+     * 选项卡组件
+     * 
+     * @constructor
+     * @extends module:Control
+     * @requires Control
+     * @exports Tabs
+     */
+    var Tabs = Control.extend(/** @lends module:Tabs.prototype */{
+
+        /**
+         * 控件类型标识
+         * 
+         * @type {string}
+         * @private
+         */
+        type: 'Tabs',
+
+        /**
+         * 控件配置项
+         * 
+         * @name module:Tabs#options
+         * @type {Object}
+         * @property {boolean} options.disabled 控件的不可用状态
+         * @property {(string | HTMLElement)} options.main 控件渲染容器
+         * @property {string} options.prefix 控件class前缀，同时将作为main的class之一
+         * @property {number} selectedIndex 默认选中项的索引值
+         * @private
+         */
+        options: {
+
+            // 提示框的不可用状态，默认为false。处于不可用状态的提示框不会出现。
+            disabled: false,
+
+            // 控件渲染主容器
+            main: '',
+
+            // 控件class前缀，同时将作为main的class之一
+            prefix: 'ecl-ui-tabs',
+
+            // 默认选中项
+            selectedIndex: 0
+        },
+
+        /**
+         * 控件初始化
+         * 
+         * @param {Object} options 控件配置项
+         * @see module:Tabs#options
+         * @private
+         */
+        init: function (options) {
+            this.main = lib.g(options.main);
+            this.labels = lib.q(options.prefix + '-labels', this.main)[0];
+            this.selectedIndex = options.selectedIndex | 0;
+
+            this.bindEvents(privates);
+        },
+
+
+        /**
+         * 绘制控件
+         * 
+         * @public
+         */
+        render: function () {
+            var options = this.options;
+
+            if (!this.rendered) {
+                this.rendered = true;
+
+                // 先给选项卡父容器添加事件监听
+                lib.on(this.labels, 'click', this._bound.onClick);
+
+                // 找出所有选项卡
+                var labels =
+                    lib.toArray(this.labels.getElementsByTagName('li'));
+
+                // 是否支持伪元素
+                var noPseudoElement = lib.browser.ie6;
+                lib.each(
+                    labels,
+                    function (label, i) {
+                        // 自动加上索引标识
+                        label.setAttribute('data-index', i);
+
+                        // 对于不支持伪元素的 ie6，自动插入一个 <i> 标签
+                        // TODO: innerHTML vs dom clone
+                        if (noPseudoElement) {
+                            label.innerHTML += '<i></i>';
+                        }
+                    }
+                );
+
+                var selectedClass = options.prefix + '-selected';
+                var selectedLabel = lib.q(selectedClass, this.labels)[0];
+
+                this.labels = labels;
+
+                if (selectedLabel) {
+                    this.selectedIndex = 
+                        selectedLabel.getAttribute('data-index') | 0;
+                }
+                else {
+                    selectedLabel = labels[this.selectedIndex];
+                    selectedLabel && lib.fire(selectedLabel, 'click');
+                }
+
+            }
+
+            return this;
+        },
+
+        /**
+         * 切换选项卡前判定
+         * 
+         * 主要用于业务逻辑判断是否允许切换选项卡，默认允许，不允许则需要明确返回 false
+         * 
+         * @param {number} oldIndex 原选中项索引值
+         * @param {number} newIndex 新选中项索引值
+         * @return {boolean} 是否允许切换
+         * @public
+         */
+        /* jshint unused:false */
+        onBeforeChange: function (oldIndex, newIndex) {
+            return true;
         }
 
 
