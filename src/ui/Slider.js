29ac6bb8517cda33f07cd14a5c9eb8f624874943
chris <wfsr@foxmail.com>
2014-04-08 00:11:03 +0800

    对外隐藏私有方法，调整文档注释

--- src/ui/Slider.js
+++ src/ui/Slider.js
@@ -37,6 +37,153 @@ define(function (require) {
     }
 
     /**
+     * 私有函数或方法
+     * 
+     * @type {Object}
+     * @namespace
+     * @name module:Slider~privates
+     */
+    var privates = /** @lends module:Slider~privates */ {
+
+        /**
+         * 清除自动播放计时器
+         * @private
+         */
+        clearSwitchTimer: function () {
+            clearTimeout(this._switchTimer);
+            this._switchTimer = 0;
+        },
+
+        /**
+         * 进入主窗口的事件
+         *
+         * @private
+         */
+        onEnter: function () {
+            if (this.options.auto) {
+                privates.clearSwitchTimer.call(this);
+            }
+        },
+
+        /**
+         * 离开主窗口的事件
+         *
+         * @private
+         */
+        onLeave: function () {
+            //如果使用自动轮播，则触发轮播计时
+            if (this.options.auto) {
+                this.play();
+            }
+        },
+
+        /**
+         * 自动切换处理事件
+         * @private
+         */
+        onSwitch: function () {
+            this.next();
+            this.play();
+        },
+
+        /**
+         * 上一个按钮点击事件
+         *
+         * @private
+         */
+        onPrevClick: function () {
+            var me = this;
+            if (!me._switchDelayTimer) {
+                me._switchDelayTimer = setTimeout(function () {
+                    privates.clearSwitchDelayTimer.call(me);
+                    me.prev();
+                }, me.options.switchDelay);
+            }
+        },
+
+        /**
+         * 下一个按钮点击事件
+         *
+         * @private
+         */
+        onNextClick: function () {
+            var me = this;
+            if (!me._switchDelayTimer) {
+                me._switchDelayTimer = setTimeout(function () {
+                    privates.clearSwitchDelayTimer.call(me);
+                    me.next();
+                }, me.options.switchDelay);
+            }
+        },
+
+        /**
+         * 索引的点击事件
+         *
+         * @param {HTMLEvent} e dom事件
+         * @private
+         */
+        onIndexClick: function (e) {
+            var me = this;
+            var target = lib.getTarget(e);
+            if (target['data-index'] !== '' && !me._switchDelayTimer) {
+                var index = target.getAttribute('data-index');
+                me._switchDelayTimer = setTimeout(function () {
+                    privates.clearSwitchDelayTimer.call(me);
+                    me.go(+index);
+                }, me.options.switchDelay);
+            }
+        },
+
+        /**
+         * 清除切换延迟
+         *
+         * @private
+         */
+        clearSwitchDelayTimer: function () {
+            clearTimeout(this._switchDelayTimer);
+            this._switchDelayTimer = 0;
+        },
+
+        /**
+         * 切换到当前索引，设置选中项目
+         * 
+         * @private
+         */
+        setCurrent: function () {
+            var opt = this.options;
+
+            //如果不是循环模式，则设置prev按钮为不可点击
+            if (opt.prevElement) {
+                lib[
+                    this.index === 0 && !opt.circle ? 'addClass' : 'removeClass'
+                ](opt.prevElement, this.getClass('prev-disable'));
+            }
+
+            //如果不是循环模式，则设置next按钮为不可点击
+            if (opt.nextElement) {
+                lib[
+                    this.index === this.count - 1 && !opt.circle
+                    ? 'addClass'
+                    : 'removeClass'
+                ](opt.nextElement, this.getClass('next-disable'));
+            }
+
+            //选中索引条目
+            if (opt.indexElment) {
+                var elements = getChildren(opt.indexElment);
+                elements[this.lastIndex] && lib.removeClass(
+                elements[this.lastIndex], this.getClass('index-selected'));
+                elements[this.index] && lib.addClass(
+                elements[this.index], this.getClass('index-selected'));
+            }
+
+        },
+
+
+    };
+
+
+    /**
      * 轮播组件
      *
      * 提供图片以及滚动框的轮播
@@ -77,34 +224,34 @@ define(function (require) {
          * @property {(string | HTMLElement)} options.main 控件渲染容器
          *
          * @property {HTMLElement=} options.stage 控件动画容器，
-         *      如果不设则按class规则查找`options.prefix` + `stage`
+         * 如果不设则按class规则查找`options.prefix` + `stage`
          *
          * @property {HTMLElement=} options.prevElement prev按钮的容器，
-         *      如果不设则按class规则查找`options.prefix` + `prev`
+         * 如果不设则按class规则查找`options.prefix` + `prev`
          *
          * @property {HTMLElement=} options.nextElement next按钮的容器，
-         *      如果不设则按class规则查找`options.prefix` + `next`
+         * 如果不设则按class规则查找`options.prefix` + `next`
          *
          * @property {HTMLElement=} options.indexElment 轮播索引按钮的容器，
-         *      会将第一级子元素设为索引元素，
-         *      如果不设则按class规则查找`options.prefix` + `index`
+         * 会将第一级子元素设为索引元素，
+         * 如果不设则按class规则查找`options.prefix` + `index`
          *
          * @property {boolean} options.auto 是否自动轮播
          * @property {boolean} options.circle 是否播放到结尾时回到起始，
-         *      在自动轮播下，需要设置为true
+         * 在自动轮播下，需要设置为true
          * @property {Number} options.autoInterval 自动切换动画的延迟时间
          * @property {Number} options.switchDelay 点击切换索引的延迟时间
          * @property {Function} options.onChange 当播放索引改变时的事件
          * @property {string} options.prefix 控件class前缀，同时将作为main的class之一
          *
          * @property {string} options.anim 使用的轮播动画，
-         *      默认提供`no`,`slide`,`opacity`
+         * 默认提供`no`,`slide`,`opacity`
          * @property {Object} options.animOptions 轮播动画选项，
-         *      不同的动画效果配置可能不一样
+         * 不同的动画效果配置可能不一样
          * @property {string} options.animOptions.easing 使用的动画算子
          * @property {Number} options.animOptions.interval 每次动画时间间隔
          * @property {string} options.animOptions.direction 滑动门的滚动方向
-         *      `horizontal` or `vertical`
+         * `horizontal` or `vertical`
          *
          *
          * @private
@@ -162,22 +309,63 @@ define(function (require) {
         },
 
         /**
-         * 需要绑定 this 的方法名，多个方法以半角逗号分开
+         * 控件初始化
          *
-         * @type {string}
+         * @param {Object} options 控件配置项
+         * @see module:Pager#options
          * @private
          */
-        binds: ''
-            + 'onEnter,onLeave,'
-            + 'switchHandler,onPrevClick,'
-            + 'onNextClick,onIndexClick',
+        init: function (options) {
+
+            this._disabled = options.disabled;
+
+            this.bindEvents(privates);
+
+            var bound = this._bound;
+
+            if (options.main) {
+                this.main = lib.g(options.main);
+                lib.addClass(this.main, options.prefix);
+                lib.on(this.main, 'mouseenter', bound.onEnter);
+                lib.on(this.main, 'mouseleave', bound.onLeave);
+
+                //根据class查找未知的元素
+                options.stage = lib.g(options.stage) || lib.q(this.getClass('stage'), this.main)[0];
+
+                //根据class查找未知的元素
+                options.prevElement = lib.g(options.prevElement) || this.query(this.getClass('prev'))[0];
+
+                options.nextElement = lib.g(options.nextElement) || this.query(this.getClass('next'))[0];
+
+                options.indexElment = lib.g(options.indexElment) || this.query(this.getClass('index'))[0];
+
+                if (options.prevElement) {
+                    lib.on(options.prevElement, 'click', bound.onPrevClick);
+                }
+
+                if (options.nextElement) {
+                    lib.on(options.nextElement, 'click', bound.onNextClick);
+                }
+
+                if (options.indexElment) {
+                    lib.on(options.indexElment, 'click', bound.onIndexClick);
+                }
+
+                //设置当前的动画组件
+                var AnimClass = typeof options.anim === 'string'
+                    ? Anim.anims[options.anim]
+                    : options.anim;
+                this.curAnim = new AnimClass(this, options.animOptions);
+            }
+        },
+
 
         /**
          * 根据名字构建的css class名称
          *
          * @param {string} name 模块名字
          * @return {string} 构建的class名称
-         * @private
+         * @public
          */
         getClass: function (name) {
             name = name ? '-' + name : '';
@@ -185,52 +373,11 @@ define(function (require) {
         },
 
         /**
-         * 清除自动播放计时器
-         * @private
-         */
-        clearSwitchTimer: function () {
-            clearTimeout(this.switchTimer);
-            this.switchTimer = 0;
-        },
-
-        /**
-         * 进入主窗口的事件
-         *
-         * @private
-         */
-        onEnter: function () {
-            if (this.options.auto) {
-                this.clearSwitchTimer();
-            }
-        },
-
-        /**
-         * 离开主窗口的事件
-         *
-         * @private
-         */
-        onLeave: function () {
-            //如果使用自动轮播，则触发轮播计时
-            if (this.options.auto) {
-                this.play();
-            }
-        },
-
-        /**
-         * 自动切换处理事件
-         * @private
-         */
-        switchHandler: function () {
-            this.next();
-            this.play();
-        },
-
-        /**
          * 获得轮播的索引
          *
          * @param {Number} index 设置的索引
          * @return {Number} 计算后的索引
-         * @private
+         * @public
          */
         getIndex: function (index) {
 
@@ -257,149 +404,6 @@ define(function (require) {
             return goTo;
         },
 
-        /**
-         * 上一个按钮点击事件
-         *
-         * @private
-         */
-        onPrevClick: function () {
-            var me = this;
-            if (!me.switchDelayTimer) {
-                me.switchDelayTimer = setTimeout(function () {
-                    me.clearSwitchDelayTimer();
-                    me.prev();
-                }, me.options.switchDelay);
-            }
-        },
-
-        /**
-         * 下一个按钮点击事件
-         *
-         * @private
-         */
-        onNextClick: function () {
-            var me = this;
-            if (!me.switchDelayTimer) {
-                me.switchDelayTimer = setTimeout(function () {
-                    me.clearSwitchDelayTimer();
-                    me.next();
-                }, me.options.switchDelay);
-            }
-        },
-
-        /**
-         * 索引的点击事件
-         *
-         * @param {HTMLEvent} e dom事件
-         * @private
-         */
-        onIndexClick: function (e) {
-            var me = this;
-            var target = lib.getTarget(e);
-            if (target['data-index'] !== '' && !me.switchDelayTimer) {
-                var index = target.getAttribute('data-index');
-                me.switchDelayTimer = setTimeout(function () {
-                    me.clearSwitchDelayTimer();
-                    me.go(+index);
-                }, me.options.switchDelay);
-            }
-        },
-
-        /**
-         * 清除切换延迟
-         *
-         * @private
-         */
-        clearSwitchDelayTimer: function () {
-            clearTimeout(this.switchDelayTimer);
-            this.switchDelayTimer = 0;
-        },
-
-        /**
-         * 切换到当前索引，设置选中项目
-         * @private
-         */
-        setCurrent: function () {
-            var opt = this.options;
-
-            //如果不是循环模式，则设置prev按钮为不可点击
-            if (opt.prevElement) {
-                lib[
-                this.index === 0 && !opt.circle ? 'addClass' : 'removeClass'](
-                opt.prevElement, this.getClass('prev-disable'));
-            }
-
-            //如果不是循环模式，则设置next按钮为不可点击
-            if (opt.nextElement) {
-                lib[
-                    this.index === this.count - 1 && !opt.circle
-                    ? 'addClass'
-                    : 'removeClass'
-                ](opt.nextElement, this.getClass('next-disable'));
-            }
-
-            //选中索引条目
-            if (opt.indexElment) {
-                var elements = getChildren(opt.indexElment);
-                elements[this.lastIndex] && lib.removeClass(
-                elements[this.lastIndex], this.getClass('index-selected'));
-                elements[this.index] && lib.addClass(
-                elements[this.index], this.getClass('index-selected'));
-            }
-
-        },
-
-        /**
-         * 控件初始化
-         *
-         * @param {Object} options 控件配置项
-         * @see module:Pager#options
-         * @private
-         */
-        init: function (options) {
-
-            this.disabled = options.disabled;
-
-            if (options.main) {
-                this.main = lib.g(options.main);
-                lib.addClass(this.main, options.prefix);
-                lib.on(this.main, 'mouseenter', this.onEnter);
-                lib.on(this.main, 'mouseleave', this.onLeave);
-
-                //根据class查找未知的元素
-                options.stage = lib.g(options.stage)
-                    || lib.q(this.getClass('stage'), this.main)[0];
-
-                //根据class查找未知的元素
-                options.prevElement = lib.g(options.prevElement)
-                    || lib.q(this.getClass('prev'), this.main)[0];
-
-                options.nextElement = lib.g(options.nextElement)
-                    || lib.q(this.getClass('next'), this.main)[0];
-
-                options.indexElment = lib.g(options.indexElment)
-                    || lib.q(this.getClass('index'), this.main)[0];
-
-                if (options.prevElement) {
-                    lib.on(options.prevElement, 'click', this.onPrevClick);
-                }
-
-                if (options.nextElement) {
-                    lib.on(options.nextElement, 'click', this.onNextClick);
-                }
-
-                if (options.indexElment) {
-                    lib.on(options.indexElment, 'click', this.onIndexClick);
-                }
-
-                //设置当前的动画组件
-                var AnimClass = typeof options.anim === 'string'
-                    ? Anim.anims[options.anim]
-                    : options.anim;
-                this.curAnim = new AnimClass(
-                this, options.animOptions);
-            }
-        },
 
         /**
          * 如果是自动播放，则激活轮播
@@ -407,9 +411,8 @@ define(function (require) {
          */
         play: function () {
             if (this.options.auto) {
-                this.clearSwitchTimer();
-                this.switchTimer = setTimeout(
-                this.switchHandler, this.options.autoInterval);
+                privates.clearSwitchTimer.call(this);
+                this._switchTimer = setTimeout(this._bound.onSwitch, this.options.autoInterval);
             }
         },
 
@@ -427,16 +430,20 @@ define(function (require) {
 
             //设置item样式
             lib.each(
-            childNodes, function (item) {
-                lib.addClass(item, me.getClass('item'));
-            });
+                childNodes,
+                function (item) {
+                    lib.addClass(item, me.getClass('item'));
+                }
+            );
 
             //设置索引项目
             if (opt.indexElment) {
                 lib.each(
-                getChildren(opt.indexElment), function (item, index) {
-                    item.setAttribute('data-index', index);
-                });
+                    getChildren(opt.indexElment),
+                    function (item, index) {
+                        item.setAttribute('data-index', index);
+                    }
+                );
             }
 
             me.stage = opt.stage;
@@ -445,7 +452,7 @@ define(function (require) {
             me.stageWidth = opt.stage.clientWidth;
             me.stageHeight = opt.stage.clientHeight;
 
-            me.setCurrent();
+            privates.setCurrent.call(this);
             me.curAnim.refresh();
         },
 
@@ -505,14 +512,12 @@ define(function (require) {
                 this.lastIndex = this.index;
                 this.index = goTo;
 
-                this.setCurrent();
+                privates.setCurrent.call(this);
 
                 var event = {
                     index: goTo,
                     lastIndex: this.lastIndex
                 };
-
-                this.options.onChange && this.options.onChange(event);
 
                 /**
                  * @event module:Slider#change
@@ -525,35 +530,38 @@ define(function (require) {
 
         /**
          * 销毁，注销事件，解除引用
+         * 
+         * @fires module:Slider#dispose
          * @override
          * @public
-         * @fires module:Slider#dispose
          */
         dispose: function () {
             //停止动画
-            this.clearSwitchDelayTimer();
-            this.clearSwitchTimer();
+            privates.clearSwitchDelayTimer.call(this);
+            privates.clearSwitchTimer.call(this);
 
             this.curAnim.dispose();
             this.curAnim = null;
 
+            var bound = this._bound;
+
             //注销按钮事件
             var options = this.options;
             if (options.prevElement) {
-                lib.un(options.prevElement, 'click', this.onPrevClick);
+                lib.un(options.prevElement, 'click', bound.onPrevClick);
             }
 
             if (options.nextElement) {
-                lib.un(options.nextElement, 'click', this.onNextClick);
+                lib.un(options.nextElement, 'click', bound.onNextClick);
             }
 
             if (options.indexElment) {
-                lib.un(options.indexElment, 'click', this.onIndexClick);
+                lib.un(options.indexElment, 'click', bound.onIndexClick);
             }
 
             //注销舞台事件
-            lib.un(this.main, 'mouseenter', this.onEnter);
-            lib.un(this.main, 'mouseleave', this.onLeave);
+            lib.un(this.main, 'mouseenter', bound.onEnter);
+            lib.un(this.main, 'mouseleave', bound.onLeave);
 
             this.main = this.stage = this.options = null;
 
