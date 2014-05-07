84dcfc9628fdf675f76d17411b83eb6b7c785c96
Feng Weifeng <jpssff@gmail.com>
2014-05-07 21:04:09 +0800

    add ps www online support

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function (grunt) {
     
-    var build_prefix = grunt.option('prefix') || 'ecomui';
+    var buildPrefix = grunt.option('prefix') || 'ui';
     var pkg = grunt.file.readJSON('package.json');
 
     grunt.initConfig({
@@ -16,7 +16,10 @@ module.exports = function (grunt) {
         clean: {
             before: ['asset', 'bin'],
             after: ['src/moye'],
-            afterdoc: ['example/css']
+            afterdoc: ['example/css'],
+            //真实上线后。lib已经和Control合并，并且paths已经配置lib->Control，所以
+            //编译完成后，直接删除lib.js
+            'after-online': ['asset/online/ui/lib.js']
         },
         
         jshint: {
@@ -31,6 +34,18 @@ module.exports = function (grunt) {
                     cwd: 'src/css',
                     src: '*.less',
                     dest: 'asset/css',
+                    ext: '.css'
+                }]
+            },
+            online: {
+                options: {
+                    compress: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/css',
+                    src: '*.less',
+                    dest: 'asset/online/ui',
                     ext: '.css'
                 }]
             }
@@ -70,7 +85,7 @@ module.exports = function (grunt) {
                 expand: true,
                 cwd: '<%=meta.src.main%>/ui',
                 src: '**',
-                dest: '<%=meta.src.main%>/moye/' + build_prefix,
+                dest: '<%=meta.src.main%>/moye/' + buildPrefix,
                 flatten: false,
                 filter: 'isFile',
             },
@@ -108,12 +123,25 @@ module.exports = function (grunt) {
                     generateSourceMaps: false,
                     optimize: 'uglify2'/*,
                     modules: [
-                        {name: build_prefix + '/Calendar'},
-                        {name: build_prefix + '/City'}
+                        {name: 'Calendar'},
+                        {name: 'City'}
                     ]*/
                 }
             },
 
+            online: {
+                options: {
+                    baseUrl: '<%=meta.src.main%>/moye',
+                    dir: 'asset/online/',
+                    skipDirOptimize: false,
+                    preserveLicenseComments: false,
+                    generateSourceMaps: false,
+                    optimize: 'uglify2',
+                    modules: [
+                        {name: buildPrefix + '/Control'},
+                    ]
+                }
+            }
         },
 
         connect: {
@@ -144,10 +172,21 @@ module.exports = function (grunt) {
                     }
                 }
             },
+            online: {
+                src: './asset/online/ui/*.js',
+                options: {
+                    outfile: 'SpecRunner.html',
+                    keepRunner: true,
+                    specs: 'test/spec/*Spec.js',
+                    vendor: [],
+                    host: 'http://localhost:<%= connect.test.options.port %>',
+                    template: 'test/templates/OnlineRunner.tmpl'
+                }
+            },
             istanbul: {
                 src: './<%=meta.src.main%>/*/*.js',
                 options: {
-                    specs: ['test/spec/*Spec.js'],
+                    specs: '<%= jasmine.requirejs.options.specs %>',
                     vendor: [],
                     outfile: 'SpecRunner.html',
                     keepRunner: true,
@@ -170,23 +209,22 @@ module.exports = function (grunt) {
             }
         },
 
-        karma: {
-            options: {
-                configFile: 'test/config.js',
-                reporters: 'dots',
-                singleRun: true
-            },
-            dev: {
-                browsers: ['PhantomJS']
-            }
-        },
-
         'gh-pages': {
             options: {
                 base: './',
                 message: 'Auto-generated commit by grunt'
             },
             src: ['doc/api/*', 'example/**', 'src/*/*']
+        },
+
+        'join-css-js': {
+            online: {
+                files: [{
+                    expand: true,
+                    cwd: 'asset/online/ui',
+                    src: '*.js'
+                }]
+            }
         }
 
     });
@@ -202,12 +240,43 @@ module.exports = function (grunt) {
         }
     );
 
+    //合并组件的js、css文件，这样模板开发者就不用再关心css的引入问题
+    grunt.registerMultiTask('join-css-js', 'join ui css and js', function(){
+        this.files.forEach(function(file){
+            var files = file.src.filter(function(filePath) {
+                var cssPath = filePath.replace(/\.js$/, '.css');
+                if (!grunt.file.exists(filePath) || !grunt.file.exists(cssPath)) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }).map(function(filePath) {
+                var cssPath = filePath.replace(/\.js$/, '.css');
+                var js = grunt.file.read(filePath),
+                    css = grunt.file.read(cssPath);
+                var content = "A.insertCss(" + JSON.stringify(css) + ");";
+                content += js;
+                grunt.file.write(filePath, content);
+                grunt.file.delete(cssPath);
+                grunt.log.writeln(filePath + ' join css and js completed.');
+                return filePath;
+            });
+        });
+        debugger;
+    });
 
-    grunt.registerTask('base', ['clean:before', 'jshint', 'lesslint', 'less', 'csslint']);
+
+    grunt.registerTask('base', ['clean:before', 'jshint', 'lesslint', 'less:compile', 'csslint']);
+    grunt.registerTask('base-online', ['clean:before', 'jshint', 'lesslint', 'csslint']);
+
     grunt.registerTask('build', ['base', 'copy:build', 'requirejs:build', 'clean:after']);
     grunt.registerTask('test', ['base', 'connect', 'jasmine:requirejs']);
     grunt.registerTask('cover', ['base', 'connect', 'jasmine:istanbul']);
     grunt.registerTask('default', ['base']);
-    grunt.registerTask('page', ['less', 'copy:doc', 'jsdoc', 'gh-pages', 'clean:afterdoc']);
+    grunt.registerTask('example', ['less', 'copy:doc']);
+    grunt.registerTask('page', ['example', 'jsdoc', 'gh-pages', 'clean:afterdoc']);
 
+    grunt.registerTask('build-online', ['base-online', 'copy:build', 'requirejs:online', 'less:online',
+                       'join-css-js:online', 'clean:after', 'clean:after-online']);
+    grunt.registerTask('test-online', ['build-online', 'connect', 'jasmine:online']);
 }
